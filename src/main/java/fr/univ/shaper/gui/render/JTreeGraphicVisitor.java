32be9b6212e7623dafe8707e413c35e27468f549
David <david.deschamps@etu.univ-rouen.fr>
2021-04-21 18:21:13 +0200

    commit 2

--- src/main/java/fr/univ/shaper/gui/render/JTreeGraphicVisitor.java
+++ src/main/java/fr/univ/shaper/gui/render/JTreeGraphicVisitor.java
@@ -1,2 +1,71 @@
-package fr.univ.shaper.gui.render;public class JTreeGraphicVisitor {
+package fr.univ.shaper.gui.render;
+
+import fr.univ.shaper.core.GraphicVisitor;
+import fr.univ.shaper.core.element.Layer;
+import fr.univ.shaper.core.element.noisy.NoisyCircle;
+import fr.univ.shaper.core.element.noisy.NoisyLine;
+import fr.univ.shaper.core.element.noisy.NoisyRectangle;
+import fr.univ.shaper.core.element.perfect.PerfectCircle;
+import fr.univ.shaper.core.element.perfect.PerfectLine;
+import fr.univ.shaper.core.element.perfect.PerfectRectangle;
+import fr.univ.shaper.util.Contract;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.util.Stack;
+
+public class JTreeGraphicVisitor implements GraphicVisitor {
+
+    private final Stack<DefaultMutableTreeNode> stack;
+
+    public JTreeGraphicVisitor(DefaultMutableTreeNode root) {
+        Contract.assertThat(root != null, "L'élément racine root ne doit pas être null");
+        this.stack = new Stack<>();
+        this.stack.push(root);
+    }
+
+    @Override
+    public void visitLayer(Layer layer) {
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(layer);
+        stack.peek().add(node);
+        stack.push(node);
+        layer.getChildren().forEach(e -> e.accept(this));
+        stack.pop();
+    }
+
+    @Override
+    public void visitPerfectRectangle(PerfectRectangle element) {
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(element, false);
+        stack.peek().add(node);
+    }
+
+    @Override
+    public void visitPerfectCircle(PerfectCircle element) {
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(element, false);
+        stack.peek().add(node);
+    }
+
+    @Override
+    public void visitPerfectLine(PerfectLine element) {
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(element, false);
+        stack.peek().add(node);
+    }
+
+    @Override
+    public void visitNoisyRectangle(NoisyRectangle element) {
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(element, false);
+        stack.peek().add(node);
+    }
+
+    @Override
+    public void visitNoisyCircle(NoisyCircle element) {
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(element, false);
+        stack.peek().add(node);
+    }
+
+    @Override
+    public void visitNoisyLine(NoisyLine element) {
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(element, false);
+        stack.peek().add(node);
+    }
 }
--- src/main/java/fr/univ/shaper/gui/view/LayerPanel.java
+++ src/main/java/fr/univ/shaper/gui/view/LayerPanel.java
@@ -1,10 +1,18 @@
 package fr.univ.shaper.gui.view;
 
 
+import fr.univ.shaper.core.GraphicElement;
+import fr.univ.shaper.core.GraphicVisitor;
+import fr.univ.shaper.core.element.GraphicFactory;
+import fr.univ.shaper.core.element.Layer;
+import fr.univ.shaper.core.element.perfect.PerfectGraphicFactory;
 import fr.univ.shaper.gui.controller.DrawController;
+import fr.univ.shaper.gui.render.JTreeGraphicVisitor;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import java.awt.*;
 
 public class LayerPanel extends JPanel {
 
@@ -12,17 +20,62 @@ public class LayerPanel extends JPanel {
     private JTree tree;
 
     public LayerPanel(DrawController controller) {
-        //create the root node
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Layer principale");
-        //create the child nodes
-        DefaultMutableTreeNode vegetableNode = new DefaultMutableTreeNode("Vegetables");
-        DefaultMutableTreeNode fruitNode = new DefaultMutableTreeNode("Fruits");
-        //add the child nodes to the root node
-        root.add(vegetableNode);
-        root.add(fruitNode);
-
-        //create the tree by passing in the root node
-        tree = new JTree(root);
+        tree = new JTree(new DefaultMutableTreeNode());
         add(tree);
+        performJTree();
+        ((DefaultTreeModel)tree.getModel()).reload();
+    }
+
+    public void performJTree() {
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+        GraphicVisitor gv = new JTreeGraphicVisitor((DefaultMutableTreeNode) tree.getModel().getRoot());
+        getDemoGroups().accept(gv);
+    }
+
+
+
+    public static GraphicElement getDemoGroups() {
+        GraphicFactory factory = new PerfectGraphicFactory();
+        Layer result = factory.createLayer();
+        Layer person =factory.createLayer();
+        Layer rest = factory.createLayer();
+
+
+        rest.append(factory.createLine(0, 500, 800, 500, Color.GREEN));
+        rest.append(factory.createLine(300, 0, 0, 300, Color.YELLOW));
+
+        rest.append(factory.createLine(30, 300, 180, 200, Color.BLUE));
+        rest.append(factory.createLine(330, 300, 180, 200, Color.BLUE));
+        rest.append(factory.createRectangle(30, 300,330, 500, Color.RED));
+
+        double sunX = 600;
+        double sunY = 120;
+        double sunRad = 60;
+        rest.append(factory.createCircle(sunX, sunY, sunRad, Color.BLACK));
+        int sunRay = 20;
+        for (int i=0; i<sunRay; ++i) {
+            double tau=i*2*Math.PI/sunRay;
+            rest.append(factory.createLine(sunX+(sunRad+5)*Math.cos(tau),
+                    sunY-(sunRad+5)*Math.sin(tau),
+                    sunX+(1.5*sunRad+5)*Math.cos(tau),
+                    sunY-(1.5*sunRad+5)*Math.sin(tau),
+                    Color.BLACK));
+        }
+
+        double manX=600;
+        double manY=450;
+        person.append(factory.createLine(manX, manY-70, manX-40, manY-110, Color.RED));
+        person.append(factory.createLine(manX, manY-70, manX+40, manY-110, Color.RED));
+        person.append(factory.createCircle(manX, manY-120, 20, Color.GRAY));
+        person.append(factory.createLine(manX, manY, manX, manY-100, Color.BLUE));
+        person.append(factory.createLine(manX, manY, manX-20, manY+50, Color.BLACK));
+        person.append(factory.createLine(manX, manY, manX+20, manY+50, Color.BLACK));
+
+        //person.move(-400, 0);
+
+        result.append(rest);
+        result.append(person);
+
+        return result;
     }
 }
